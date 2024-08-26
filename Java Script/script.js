795fdad533ab979b52e70ff82905cfd110a1bd99
Gabryel da Silva Moura <129227189+GabryelSilvaMoura@users.noreply.github.com>
2024-08-26 18:10:10 -0300

    Botão subir

--- Java Script/script.js
+++ Java Script/script.js
@@ -1,1 +1,16 @@
 // MENU
+function clicou(){
+    if (items.style.display == 'block') {
+        items.style.display = 'none'
+    } else{
+        items.style.display = 'block'
+    }
+}
+
+    function mudouTamanho(){
+        if(window.innerWidth >= 760){
+            items.style.display = 'flex'
+        } else{
+            items.style.display = 'none'
+        }
+    }
