5e8dd67462d5cac09bc1c0e9ff88668401b49747
Gabryel da Silva Moura <129227189+GabryelSilvaMoura@users.noreply.github.com>
2024-08-31 14:40:45 -0300

    Personalizando

--- Java Script/script.js
+++ Java Script/script.js
@@ -1,4 +1,6 @@
-// MENU
+// Java Script da Tela Home
+
+// MENU: Aparecer e sumir os itens do menu
 function clicou(){
     if (items.style.display == 'block') {
         items.style.display = 'none'
@@ -7,10 +9,15 @@ function clicou(){
     }
 }
 
-    function mudouTamanho(){
-        if(window.innerWidth >= 760){
-            items.style.display = 'flex'
-        } else{
-            items.style.display = 'none'
-        }
+function mudouTamanho(){
+    if(window.innerWidth >= 760){
+        items.style.display = 'flex'
+    } else{
+        items.style.display = 'none'
     }
+}
+
+
+// Aparecer informações dos Cards
+passou = document.getElementsByClassName("info")
+
